8df6395bfe8fbb18d3a4856084f92f304c96479b
Ciont Antonia <ciontantonia@yahoo.com>
2024-03-31 16:41:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
+import TopNav from "./Components/TopNav";
+import CatNav from "./Components/CatNav";
+import MainComponent from "./Components/MainComponent";
 
 function App() {
   return (
     <div className="App">
-      <div className="header bg-dark">
-        <div className="row">
-          <div className="brand my-1">
-            <h1>eStore</h1>
-          </div>
-          <div className="inp-container p-0 my-4 w-50 h-25 bg-white">
-            <input className="form-control" placeholder="Search" />
-            <button>
-              <i className="fa fa-search" />
-            </button>
-          </div>
-        </div>
-      </div>
+      <TopNav />
+      <CatNav />
+      <MainComponent />
     </div>
   );
 }
