fef3855e3e146c299077f856d6ccda35d78d38b5
Ciont Antonia <ciontantonia@yahoo.com>
2024-03-31 18:09:55 +0300

    third commit-MySQL established

--- estoreServer/index.js
+++ estoreServer/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 
-const server = app.listen(5003, () => {
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password9898@",
+  database: "estore2",
+  port: 3306,
+  multipleStatements: true,
+});
+
+app.get("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("Connection Established");
+    }
+  });
+});
+
+const PORT = 5003;
+
+const server = app.listen(PORT, () => {
   console.log("App is running on the port 5003");
 });
